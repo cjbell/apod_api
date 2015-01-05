af80fab1fc078ed9e235d9242a4a828e14be0b0c
Chris Bell <cjbell1988@gmail.com>
2015-01-05 14:34:51 +0000

    Modified date format in post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,4 +2,8 @@ class PostSerializer < ActiveModel::Serializer
   attributes :name, :description_html, :credit_html,
              :small_image_url, :full_image_url, :video_embed_url,
              :original_url, :published_at
+
+  def published_at
+    object.published_at.strftime("%Y-%m-%d")
+  end
 end
