986b6743807b3060d5b531096e960938420445d7
Chris Bell <cjbell1988@gmail.com>
2015-06-10 11:33:59 -0400

    Tweaks

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,9 +1,25 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :name, :description_html, :credit_html,
+  include ActionView::Helpers::SanitizeHelper
+
+
+  attributes :name, :description, :credit,
              :small_image_url, :full_image_url, :video_embed_url,
              :original_url, :published_at
 
   def published_at
     object.published_at.strftime("%Y-%m-%d")
+  end
+
+  def description
+    strip_tags(object.description_html)
+      .sub('Explanation:', '')
+      .gsub("\n", ' ')
+      .gsub('  ', "\n")
+      .strip
+  end
+
+  def credit
+    strip_tags(object.credit_html).strip
+      .gsub('&amp;', "&")
   end
 end
